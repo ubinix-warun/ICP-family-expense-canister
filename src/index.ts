7d76db03dc57d0297e99f0dda70a47259c4f6364
Vitomir Pavlov <vitomirpavlov@gmail.com>
2023-09-04 09:58:04 +0300

    main - return back my original code and implement only the relevant changes from the PR: add createdBy field that is will get the caller principal in the Family record; remove the familyName and keep only the familyId; make more explicit check for the family existance by only Family.get(); add gitignore file for the deployed/built files and directories;

--- src/index.ts
+++ src/index.ts
@@ -1,183 +1,125 @@
-import {
-  $query,
-  $update,
-  Record,
-  StableBTreeMap,
-  Vec,
-  match,
-  Result,
-  nat64,
-  ic,
-  Opt,
-  float64,
-  Principal
-} from "azle";
-import { v4 as uuidv4 } from "uuid";
+import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt, Principal } from 'azle';
+import { v4 as uuidv4 } from 'uuid';
 
 type Family = Record<{
-  id: string;
-  name: string;
-  members: Vec<string>;
-  address: string;
-  createdBy: Principal; // Add the creator's principal
-  createdAt: nat64;
-  updatedAt: Opt<nat64>;
-}>;
+    id: string;
+    name: string; // * The family name
+    members: Vec<string>; // * Names of the family members, e.g., George
+    address: string; // * The address of the property where they live
+    createdBy: Principal; // * Add the creator's principal
+    createdAt: nat64;
+    updatedAt: Opt<nat64>;
+}>
 
-type FamilyPayload = Record<{
-  name: string;
-  members: Vec<string>;
-  address: string;
-}>;
+type FamilyPayload = Record <{
+    name: string;
+    members: Vec<string>;
+    address: string;
+}>
 
 type FamilyExpense = Record<{
-  id: string;
-  familyId: string;
-  amount: string;
-  attachmentURL: string;
-  createdAt: nat64;
-}>;
+    id: string;
+    familyId: string;
+    amount: string;
+    attachmentURL: string; // * Picture of a receipt
+    createdAt: nat64;
+}>
 
 type FamilyExpensePayload = Record<{
-  familyId: string;
-  amount: string;
-  attachmentURL: string;
-}>;
+    familyId: string;
+    amount: string;
+    attachmentURL: string;
+}>
 
 const familyStorage = new StableBTreeMap<string, Family>(0, 44, 1024);
-const familyExpensesStorage = new StableBTreeMap<string, FamilyExpense>(
-  1,
-  44,
-  1024
-);
+const familyExpensesStorage = new StableBTreeMap<string, FamilyExpense>(1, 44, 1024);
 
-globalThis.crypto = {
-  getRandomValues: () => {
-    const array = new Uint8Array(32);
-    crypto.getRandomValues(array);
-    return array;
-  }
-};
+// dfx canister call family_expense addFamily '(record {"name"= "Smith"; "members"= vec { "John"; "Jane"; "Chris"; "Kerry" }; "address"= "153 Linkoln St., DC, Washington"})'
+// dfx canister call family_expense addFamily '(record {"name"= "Wilson"; "members"= vec { "Alexander"; "Angelina"; "Mike"; "Katty" }; "address"= "810 Pleasant St., Illinois, Chicago"})'
+// dfx canister call family_expense getFamily '("abf10d57-3be8-4c0a-9c6d-f93e98cd9f08")'
+// dfx canister call family_expense getFamilies '()'
+// dfx canister call family_expense addFamilyExpense '(record {"familyId"= "f88a5706-ec6b-45e4-9fa3-3ab7c8dc2e65"; "amount"= "105.60"; "attachmentURL"= "url/path/to/some/photo/attachment"})'
+// dfx canister call family_expense addFamilyExpense '(record {"familyId"= "abf10d57-3be8-4c0a-9c6d-f93e98cd9f08"; "amount"= "255.15"; "attachmentURL"= "url/path/to/some/photo/attachment"})'
+// dfx canister call family_expense addFamilyExpense '(record {"familyId"= "f8c246f8-6167-4800-a921-72b169ec3589"; "amount"= "55.25"; "attachmentURL"= "url/path/to/some/photo/attachment"})'
+// dfx canister call family_expense getFamilyExpenses '("4f70de76-380e-43e1-b6cd-d969ec17a9f8")'
+// dfx canister call family_expense deleteFamily '("0eb5588c-aa66-416d-8a0c-0daca3ba3c3d")'
+// dfx canister call family_expense deleteFamilyExpense '("abf10d57-3be8-4c0a-9c6d-f93e98cd9f08")'
 
 $query;
 export function getFamilies(): Result<Vec<Family>, string> {
-  return Result.Ok(familyStorage.values());
+    return Result.Ok(familyStorage.values());
 }
 
 $query;
 export function getFamily(id: string): Result<Family, string> {
-  return match(familyStorage.get(id), {
-    Some: (family) => Result.Ok<Family, string>(family),
-    None: () => Result.Err<Family, string>(`Family with id=${id} not found.`)
-  });
+    return match(familyStorage.get(id), {
+        Some: (family) => Result.Ok<Family, string>(family),
+        None: () => Result.Err<Family, string>(`Family with id=${id} not found.`)
+    });
 }
 
 $query;
 export function getFamilyExpenses(familyId: string): Result<Vec<FamilyExpense>, string> {
-  const familyExpenses = familyExpensesStorage.values().filter(familyExpense =>
-    familyExpense.familyId === familyId
-  );
-  return Result.Ok(familyExpenses);
+    return Result.Ok(familyExpensesStorage.values().filter(familyExpense => familyExpense.familyId === familyId));
 }
 
 $update;
 export function addFamily(payload: FamilyPayload): Result<Family, string> {
-  const caller = ic.provisional_create_canister(
-    { controller: ic.caller() }
-  );
-  const family: Family = {
-    id: uuidv4(),
-    createdBy: caller,
-    createdAt: ic.time(),
-    updatedAt: Opt.None,
-    ...payload
-  };
-  familyStorage.insert(family.id, family);
-  return Result.Ok(family);
+    const family: Family = { id: uuidv4(), createdBy: ic.caller(), createdAt: ic.time(), updatedAt: Opt.None, ...payload };
+    familyStorage.insert(family.id, family);
+    return Result.Ok(family);
 }
 
 $update;
 export function updateFamily(id: string, payload: FamilyPayload): Result<Family, string> {
-  const caller = ic.provisional_create_canister(
-    { controller: ic.caller() }
-  );
-  return match(familyStorage.get(id), {
-    Some: (family) => {
-      if (caller != family.createdBy) {
-        return Result.Err<Family, string>(
-          `You don't have permission to update this family.`
-        );
-      }
-
-      const updatedFamily: Family = {
-        ...family,
-        ...payload,
-        updatedAt: Opt.Some(ic.time())
-      };
-      familyStorage.insert(family.id, updatedFamily);
-      return Result.Ok<Family, string>(updatedFamily);
-    },
-    None: () => Result.Err<Family, string>(`Family with id=${id} not found.`)
-  });
+    return match(familyStorage.get(id), {
+        Some: (family) => {
+            const updatedFamily: Family = {...family, ...payload, updatedAt: Opt.Some(ic.time())};
+            familyStorage.insert(family.id, updatedFamily);
+            return Result.Ok<Family, string>(updatedFamily);
+        },
+        None: () => Result.Err<Family, string>(`Couldn't update a family with id=${id}. Family not found.`)
+    });
 }
 
 $update;
 export function addFamilyExpense(payload: FamilyExpensePayload): Result<FamilyExpense, string> {
-  const family = familyStorage.get(payload.familyId);
-  if (!family) {
-    return Result.Err(`Family with id=${payload.familyId} not found.`);
-  }
+    const family = familyStorage.get(payload.familyId)
+    if (!family) {
+        return Result.Err(`Family with id=${payload.familyId} not found.`);
+    }
 
-  const familyExpense: FamilyExpense = {
-    id: uuidv4(),
-    createdAt: ic.time(),
-    ...payload
-  };
-  familyExpensesStorage.insert(familyExpense.id, familyExpense);
-  return Result.Ok(familyExpense);
+    const familyExpense: FamilyExpense = { id: uuidv4(), createdAt: ic.time(), ...payload };
+    familyExpensesStorage.insert(familyExpense.id, familyExpense);
+    return Result.Ok(familyExpense);
 }
 
 $update;
 export function deleteFamily(id: string): Result<Family, string> {
-  const caller = ic.provisional_create_canister(
-    { controller: ic.caller() }
-  );
-  const family = familyStorage.get(id);
-  if (!family) {
-    return Result.Err<Family, string>(`Family with id=${id} not found.`);
-  }
-
-  if (caller != family.createdBy) {
-    return Result.Err<Family, string>(
-      `You don't have permission to delete this family.`
-    );
-  }
-
-  familyStorage.remove(id);
-  return Result.Ok<Family, string>(family);
+    return match(familyStorage.remove(id), {
+        Some: (deletedFamily) => Result.Ok<Family, string>(deletedFamily),
+        None: () => Result.Err<Family, string>(`Couldn't delete a family with id=${id}. Family not found.`)
+    });
 }
 
 $update;
 export function deleteFamilyExpense(id: string): Result<FamilyExpense, string> {
-  const familyExpense = familyExpensesStorage.remove(id);
-  if (!familyExpense) {
-    return Result.Err<FamilyExpense, string>(
-      `Family expense with id=${id} not found.`
-    );
-  }
-  return Result.Ok<FamilyExpense, string>(familyExpense);
+    return match(familyExpensesStorage.remove(id), {
+        Some: (deletedFamilyExpense) => Result.Ok<FamilyExpense, string>(deletedFamilyExpense),
+        None: () => Result.Err<FamilyExpense, string>(`Couldn't delete a family expense with id=${id}. It has not found.`)
+    });
 }
 
-// a workaround to make uuid package work with Azle
+// A workaround to make uuid package work with Azle
 globalThis.crypto = {
     // @ts-ignore
    getRandomValues: () => {
-       let array = new Uint8Array(32)
+    let array = new Uint8Array(32)
 
-       for (let i = 0; i < array.length; i++) {
-           array[i] = Math.floor(Math.random() * 256)
-       }
+    for (let i = 0; i < array.length; i++) {
+        array[i] = Math.floor(Math.random() * 256)
+    }
 
-       return array
+    return array
    }
 }
